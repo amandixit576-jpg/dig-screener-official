1546805b17ac2e4a4df535a2ec2a1b6a0db11bb5
amandixit576-jpg <amandixit576@gmail.com>
2026-03-08 20:05:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -334,7 +334,7 @@ with col2:
             st.caption("ℹ️ *Tax estimator reflects new Indian Budget rules: 20% for STCG and 12.5% for LTCG.*")
 
 # ---> 📊 STOCK ANALYSIS ENGINE (TICKER-STYLE UI) <---
-elif st.session_state.current_view != "HOME":
+if st.session_state.current_view != "HOME":
     user_ticker = st.session_state.current_view
 
     with st.spinner("Executing Quant Models..."):
@@ -723,6 +723,7 @@ go_to_top_html = """
     </style>
 """
 st.markdown(go_to_top_html, unsafe_allow_html=True)
+
 
 
 
